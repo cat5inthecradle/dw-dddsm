6413900357a89c14b7659fe05a5d894d22c0591d
Darin Webb <dwebb@sourceallies.com>
2018-04-11 23:55:52 -0500

    change webpage text

--- app/server.js
+++ app/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOpsDays Des Moines\n');
+  res.send('<h1>Hello World</h1>');
 });
 
 const server = app.listen(PORT, HOST);
